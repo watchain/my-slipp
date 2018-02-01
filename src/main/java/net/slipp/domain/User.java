58bd2dd3e4a69e2151de4c9a6bdcfcc4d4e316d0
DESKTOP-DJ102JC\watch <watchain@yahoo.com>
2018-01-31 23:45:19 -0800

    Added Question form

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -33,6 +33,11 @@ public class User {
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
